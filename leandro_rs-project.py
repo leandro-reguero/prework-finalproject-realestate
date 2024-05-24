00d241dd50638a68a6079d30b3d4d2ccf2920cb2
leandro-reguero <lreguerolazaro@gmail.com>
2024-05-24 11:41:34 +0000

    First Commit

---
+++ leandro_rs-project.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+real_estate = pd.read_csv("/workspaces/prework-finalproject-realestate/assets/real_estate.csv")
+
+print(real_estate.head())
